648dae5c6f8cd2a4c104f5fb8220ef92520e4932
saifb <saifb@LAPTOP-CT370KF1>
2022-04-04 16:50:11 +0100

    enterpriseTest creation :

---
+++ Timesheet-spring-boot-core-data-jpa-mvc-REST-1/src/test/java/tn/esprit/spring/EntrepriseTest.java
@@ -0,0 +1,37 @@
+package tn.esprit.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.services.IEntrepriseService;
+import org.junit.Assert;
+import tn.esprit.spring.entities.Departement;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntrepriseTest {
+	@Autowired
+	private IEntrepriseService serviceEntreprise;
+
+
+	@Test
+	@Order(1)
+	public void testAjouterEntreprise() {
+		Entreprise ent = new Entreprise("SaidiJam Mat", "Agriculture");
+		int id = serviceEntreprise.ajouterEntreprise(ent);
+		Assert.assertNotNull(serviceEntreprise.getEntrepriseById(id));
+	}
+
+	@Test
+	public void testAjouterDepartment() {
+		Departement dep = new Departement("Agriculture");
+		int id = serviceEntreprise.ajouterDepartement(dep);
+		Assert.assertNotNull(serviceEntreprise.getAllDepartementsNamesByEntreprise(id));
+	}
+
+}
